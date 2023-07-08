389e4700656d02c3a27acaafaa2b67d1ad5e2472
Kartik Maheshwari <95483381+Kartik-Maheshwari@users.noreply.github.com>
2023-07-08 21:39:58 +0530

    Final Commit

---
+++ app/src/main/java/com/example/chatapp/SignUp.kt
@@ -0,0 +1,94 @@
+package com.example.chatapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+
+class SignUp : AppCompatActivity() {
+    private lateinit var edtName: EditText
+    lateinit var etEmail: EditText
+    lateinit var etConfPass: EditText
+    private lateinit var etPass: EditText
+    private lateinit var btnSignUp: Button
+    lateinit var tvRedirectLogin: TextView
+    private lateinit var mDbRef: DatabaseReference
+    // create Firebase authentication object
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        supportActionBar?.hide()
+        // View Bindings
+        edtName = findViewById(R.id.edt_name)
+        etEmail = findViewById(R.id.etSEmailAddress)
+        etConfPass = findViewById(R.id.etSConfPassword)
+        etPass = findViewById(R.id.etSPassword)
+        btnSignUp = findViewById(R.id.btnSSigned)
+        tvRedirectLogin = findViewById(R.id.tvRedirectLogin)
+
+        // Initialising auth object
+        auth = Firebase.auth
+
+        btnSignUp.setOnClickListener {
+            signUpUser()
+        }
+
+        // switching from signUp Activity to Login Activity
+        tvRedirectLogin.setOnClickListener {
+            val intent = Intent(this, LogIn::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun signUpUser() {
+        val name = edtName.text.toString()
+        val email = etEmail.text.toString()
+        val pass = etPass.text.toString()
+        val confirmPassword = etConfPass.text.toString()
+
+        // check pass
+        if (email.isBlank() || pass.isBlank() || confirmPassword.isBlank() || name.isBlank()) {
+            Toast.makeText(this, "Name, Email and Password can't be blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (pass != confirmPassword) {
+            Toast.makeText(this, "Password and Confirm Password do not match", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+        // If all credential are correct
+        // We call createUserWithEmailAndPassword
+        // using auth object and pass the
+        // email and pass in it.
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                addUserToDatabase(name,email,auth.currentUser?.uid!!)
+                Toast.makeText(this, "Successfully Singed Up", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@SignUp, MainActivity::class.java)
+                finish()
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Singed Up Failed!" + it.exception, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun addUserToDatabase(name: String, email: String, uid: String){
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+        mDbRef.child("user").child(uid).setValue(User(name, email, uid))
+    }
+}
